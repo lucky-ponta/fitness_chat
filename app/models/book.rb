faedf0a7a0f8cd624187f2eb93596fd089a9fc89
shun k <65330531+lucky-ponta@users.noreply.github.com>
2020-08-16 17:10:10 +0900

    更新

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+	belongs_to :user
+ 　 has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :books
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+
+  attachment :profile_image, destroy: false
 end
