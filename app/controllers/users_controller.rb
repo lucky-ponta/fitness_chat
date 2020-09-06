ebbc6d12cd25db75afedffa5aaa0ae9d7cc994e2
shun k <65330531+lucky-ponta@users.noreply.github.com>
2020-09-06 21:09:26 +0900

    更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,9 +32,9 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
        tags = Vision.get_image_data(@user.profile_image)
-    tags.each do |tag|
-      @user.tags.build(name: tag)
-    end
+       tags.each do |tag|
+        @user.tags.create(name: tag)
+      end
       redirect_to user_path(@user), notice: "successfully updated user!"
     else
       render "edit"
